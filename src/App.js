9aa79a261b0d9a216f6f64b93cedf209ff303e02
Kohralle <karol.komorniczak@gmail.com>
2021-02-28 01:51:05 +0100

    ability to search users added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import Navbar from './components/layout/Navbar'
+import Users from "./components/users/Users";
+import Search from "./components/users/Search";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  state = {
+    users: [],
+    loading: false
+  }
+  /* async componentDidMount(){
+    this.setState({loading: true})
+    //const res = await fetch(`https://api.github.com/users?client_id=${REACT_APP_GITHUB_CLIENT_ID}&client_secret=${REACT_APP_GITHUB_CLIENT_SECRET}`)
+    const res = await fetch('https://api.github.com/users')
+    const data = await res.json()
+    console.log(data)
+    this.setState({users: data, loading: false})
+    
+   // fetch("https://api.github.com/users").then(res => res.json()).then(data => this.setState({users: data.data, loading: false}))
+  } */
+
+  searchUsers =async (text) => {
+    //const res = await fetch(`https://api.github.com/users?client_id=${REACT_APP_GITHUB_CLIENT_ID}&client_secret=${REACT_APP_GITHUB_CLIENT_SECRET}`)
+    const res = await fetch(`https://api.github.com/search/users?q=${text}`)
+    const data = await res.json()
+    console.log(data)
+    this.setState({users: data.items, loading: false})
+    
+   // fetch("https://api.github.com/users").then(res => res.json()).then(data => this.setState({users: data.data, loading: false}))
+  } 
+  render (){
+    return (<div className="App">
+      
+      <Navbar title = 'Github Finder' icon = "fab fa-github"></Navbar>
+      <div className="container">
+        <Search searchUsers = {this.searchUsers}/>
+      <Users loading ={this.state.loading} users={this.state.users}></Users>
+      </div>
+     
+    
+  </div>);
+  }
+    
+  
 }
+
 
 export default App;
---
+++ src/components/users/Users.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import UserItem from './UserItem';
+import Spinner from'../layout/Spinner'
+import PropTypes from 'prop-types';
+
+const Users = ({users, loading}) => {
+    if(loading){
+        return <Spinner></Spinner>
+    }
+        return (
+            <div style = {userStyle}>
+                {users.map(user => (
+                    <UserItem key = {user.login} user={user}>{user.login} </UserItem>
+                ))}
+            </div>
+        )
+}
+
+Users.propTypes = {
+    users: PropTypes.array.isRequired,
+    loading: PropTypes.bool.isRequired
+}
+
+const userStyle = {
+    display: 'grid',
+    gridTemplateColumns: 'repeat(3, 1fr)',
+    gridGap: '1rem'
+}
+
+export default Users
